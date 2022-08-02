b693e408470bc77809998993ac2dcf781d2940ed
Yiğit Kılınçer <89207366+Yigitkilincr@users.noreply.github.com>
2022-08-02 13:59:29 +0300

    Added some files and updated some features.
    Hot reload has added. Made so

--- main.dart
+++ main.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-    ),
-  ),
+  home: Home()
 
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange[300],
+        ),
+        body: Center(
+          child: Text(
+            'Hello Everyone',
+            style: TextStyle(
+              fontSize: 24.0,
+              letterSpacing: 2.0,
+              fontFamily: 'Inconsolata',
+
+            ),
+          ),
+
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          // onPressed code comes here
+        },
+          backgroundColor: Colors.deepOrange,
+          child: Text('Click'),
+        ),
+      );
+  }
+}
